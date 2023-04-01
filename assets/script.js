36d4d687759f0bf265644094774c8c0d9f62c0c8
Bwhitman33 <bwhitman33@gmail.com>
2023-04-01 14:01:02 -0400

    Added some psuedo-coding to assist in getting started. Wrote some JS, got stuck.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,30 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// define each variable first?
+
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numOnly = "0123456789"
+var specChar = "!@#$%^&*()"
+
+// Need to generate functions to provide lowercase, uppercase, numbers and special characters
+function getRandomLower() {
+  var lowCase = "abcdefghijklmnopqrstuvwxyz";
+  var randomLower = lowCase[Math.floor(Math.random() * lowCase.length)];
+
+}
+
+// Need to write code to ask the user if they would like to utilize Lower Case, Capital, Numbers or Special Characters
+function confirmLower() {
+  var confirmLower= confirm("Would you like to use Lower Case letters in your password?");
+
+}
+// Write Function defining generatePassword()
+function generatePassword() {
+  var 
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
